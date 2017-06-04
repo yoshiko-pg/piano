a7651c1a83fbcabf8fc279028e0eeccc57929038
maasa <maasa.giss2@gmail.com>
2017-06-04 15:35:50 +0900

    :shirt: Fix by eslint

--- app/bundle.js
+++ app/bundle.js
@@ -1,4 +1,4 @@
-document.body.insertAdjacentHTML("beforeend",
+document.body.insertAdjacentHTML('beforeend',
   `<p>This browser does not support modules, </p>
    <p>or unsupported &lt;script nomodule&gt; attribute.</p>`);
 
