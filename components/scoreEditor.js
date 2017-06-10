582b73cde3c35001e5be7e57cd786f2b22545f3f
maasa <maasa.giss2@gmail.com>
2017-06-10 12:30:29 +0900

    Updates

--- components/scoreEditor.js
+++ components/scoreEditor.js
@@ -19,15 +19,26 @@ export default class ScoreEditorComponent extends BaseComponent {
   }
 
   async _playLine(line) {
+    let index = 0;
     for (const char of line.split('')) {
+      if (char === '-') {
+        index++;
+        continue;
+      }
+
       const event = new Event('keydown');
       event.key = char;
       window.dispatchEvent(event);
-      await this.timeout(150, () => {
+
+      const match = line.slice(index + 1).match(/^-+/) || [''];
+      const msec = 150 * (match[0].length + 1);
+
+      await this.timeout(msec, () => {
         const event = new Event('keyup');
         event.key = char;
         window.dispatchEvent(event);
-      })
+      });
+      index++;
     }
   }
 
@@ -64,10 +75,81 @@ export default class ScoreEditorComponent extends BaseComponent {
         .buttons button {
           margin: 0 10px;
         }
+        
+        button {
+          appearance: none;
+          outline: none;
+          cursor: pointer;
+          border: none;
+          padding: 8px 12px;
+          border-radius: 4px;
+          font-size: 12px;
+          text-shadow: 0 1px 1px rgba(0, 0, 0, .3);
+          color: #fff;
+          background-color: #adadad;
+          box-shadow: 0 3px 0 #7d7d7d;
+        }
+
+        button:hover {
+          background-color: #b4b4b4;
+          box-shadow: 0 2px 0 #888;
+          transform: translateY(1px);
+        }
+
+        button:active {
+          transform: translateY(3px);
+          box-shadow: none;
+        }
+        
+        button.primary {
+          background-color: #1abc9c;
+          box-shadow: 0 3px 0 #0e8c73;
+        }
+        
+        button.primary:hover {
+          background-color: #31c8aa;
+          box-shadow: 0 2px 0 #23a188;
+        }
+        
+        button.primary:active {
+          transform: translateY(3px);
+          box-shadow: none;
+        }
+        
+        #play {
+          padding: 12px 44px 12px 18px;
+          position: relative;
+          font-size: 16px;
+        }
+        
+        #play::before {
+          content: '';
+          position: absolute;
+          right: 15px;
+          top: 50%;
+          transform: translateY(-50%);
+          width: 16px;
+          height: 16px;
+          border-radius: 50%;
+          border: 2px solid #fff;
+        }
+        
+        #play::after {
+          content: '';
+          position: absolute;
+          right: 21px;
+          top: 50%;
+          transform: translateY(-50%);
+          width: 0;
+          height: 0;
+          border-style: solid;
+          border-width: 4px 0 4px 6px;
+          border-color: transparent transparent transparent #ffffff;
+        }
       </style>
 
       <div class="buttons">
-        <button id="play">Play</button>
+        <button class="primary" id="play">Play text</button>
         <button id="save">Save as URL</button>
       </div>
       <textarea>${this.value}</textarea>
